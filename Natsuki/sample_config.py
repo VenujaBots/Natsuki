f5e386be7a730cf1fa6da5e3d2ab19869cbbbfe8
Venuja Thilakarathna <91406478+VenujaBots@users.noreply.github.com>
2021-11-09 23:01:12 +0530

    Update sample_config.py

--- Natsuki/sample_config.py
+++ Natsuki/sample_config.py
@@ -5,7 +5,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/EzilaXBotV/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/MaxBot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -15,12 +15,12 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    API_ID = 7395896  # integer value, dont use ""
+    API_HASH = "cd3998ddf318dad74d7c506731bc0abc"
+    TOKEN = "2109086316:AAFuex1ZvdGrkkNxwgKTdycfBVkCGM8HTHA"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "Venuja_Sadew"
+    SUPPORT_CHAT = "Vnd Bot Supports"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001253661229
     )  # Prints any new group the bot is added to, prints just the name and ID.
@@ -29,14 +29,14 @@ class Config(object):
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = "mongodb+srv://Vndgroup:Vndgroup@cluster0.9oe9i.mongodb.net/cluster0?retryWrites=true&w=majority"  # needed for any database modules
     REDIS_URI = " "
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "VB~tIGZv0p8urCZFM4psuaJX03KN9hQFZw7twou7Fd64B~bDzX~VngaxCK_jOxcS"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
@@ -60,7 +60,7 @@ class Config(object):
     BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = (
-        "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "NMATP3T0N6WOIEBB"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
     TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
     WALL_API = (
